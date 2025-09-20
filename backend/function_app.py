6f9cf0fd3dcbff47ea9c8c3e4bc68ae1d73cb105
anhcao <anhcao4922@gmail.com>
2025-09-20 22:21:34 +0300

    [BE] (chore) improve api import

--- backend/function_app.py
+++ backend/function_app.py
@@ -1,13 +1,12 @@
 import azure.functions as func
-from src.api.auth import handle_sign_in
-from src.api.stock import get_stocks
+from src.api import auth, stock
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="auth/sign-in", methods=[func.HttpMethod.POST])
 def sign_in(req: func.HttpRequest) -> func.HttpResponse:
-    return handle_sign_in(req)
+    return auth.handle_sign_in(req)
 
 @app.route(route="stocks", methods=[func.HttpMethod.GET])
 def stocks(req: func.HttpRequest) -> func.HttpResponse:
-    return get_stocks(req)
+    return stock.get_stocks(req)
