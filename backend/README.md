8708f07418c9357212c70086f34c529a0547bd91
anhcao <anhcao4922@gmail.com>
2025-09-20 12:23:24 +0300

    [BE] (feat) init 2 apis for signin and stock

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,27 @@
 # Digital Wholesale Backend
+
+## Getting started 
+### Locally 
+Ensure you are in right directory (/backend).
+1. (Optional) create virtual environment if not exist
+```bash
+python3 -m venv .venv
+```
+
+2. Activate virtual environment
+```bash 
+# For Mac
+. .venv/bin/activate
+
+# For Windows, please check online
+```
+
+To deactivate, just run: 
+```bash 
+deactivate 
+```
+
+3. Install dependencies into virtual environment 
+```bash
+pip install -r requirements.txt
+```
--- backend/function_app.py
+++ backend/function_app.py
@@ -3,4 +3,33 @@ import datetime
 import json
 import logging
 
-app = func.FunctionApp()
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="auth/sign-in", methods=[func.HttpMethod.POST])
+def sign_in(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Received sign-in request')
+
+    client_code = req.params.get('client-code')
+
+    if client_code:
+        # Validate the client code if it valid and exists in mock db. If not valid, return 401
+        
+        # If valid, generate user a jwt token and return it to them
+        return func.HttpResponse(f"Get token for client code: {client_code}")
+    else:
+        return func.HTTP(
+             "Invalid request",
+             status_code=400
+        )
+
+@app.route(route="stocks", methods=[func.HttpMethod.GET])
+def stocks(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Received stocks request.')
+
+    # Check from cache first 
+    
+    # If user's stock does not exist in cache then get from DB
+    
+    # Get from DB then update cache to improve performance for next time 
+    # (cache should expired after midnight or when the stock data is updated in DB)
+    return func.HttpResponse(f"This http triggered function successfully")
